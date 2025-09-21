da9ae29ab988c9e871967dd73115ee9d9b8457ef
gaibianshijie666 <gaibianshijie862@gmail.com>
2025-09-21 18:44:14 +0800

    第三次提交：在线部署模块

--- src/main/java/com/chen/yuaicodemother/core/saver/CodeFileSaverTemplate.java
+++ src/main/java/com/chen/yuaicodemother/core/saver/CodeFileSaverTemplate.java
@@ -4,6 +4,7 @@ import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
 import com.chen.yuaicodemother.ai.model.enums.CodeGenTypeEnum;
+import com.chen.yuaicodemother.common.constant.AppConstant;
 import com.chen.yuaicodemother.exception.BusinessException;
 import com.chen.yuaicodemother.exception.ErrorCode;
 
@@ -18,7 +19,8 @@ import java.nio.charset.StandardCharsets;
 public abstract class CodeFileSaverTemplate<T> {
 
     // 文件保存根目录
-    protected static final String FILE_SAVE_ROOT_DIR = System.getProperty("user.dir") + "/tmp/code_output";
+    protected static final String FILE_SAVE_ROOT_DIR = AppConstant.CODE_OUTPUT_ROOT_DIR;
+
 
     /**
      * 模板方法，保存代码的标准流程（使用appid）
